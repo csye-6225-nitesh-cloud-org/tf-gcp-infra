cbf927492fb93de62308728fd390846065461db8
nitesh <more.ni@northeastern.edu>
2024-02-11 20:36:09 -0500

    Inital tf commit

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "project_name" {
+  type        = string
+  description = "project Name"
+  default     = "csye6225-webapp"
+}
+variable "region" {
+  type        = string
+  description = "region for infra"
+  default     = "us-east1"
+}
