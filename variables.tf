84cdddee2d8fa92b3da7c2edef989ed78ed68a6e
nitesh <more.ni@northeastern.edu>
2024-02-19 22:36:51 -0500

    Added Firewall

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "project_name" {
   description = "project Name"
 }
 variable "Env" {
-  type        = string
+  type = string
 }
 variable "region" {
   type        = string
@@ -21,3 +21,74 @@ variable "private-subnet-name" {
 variable "public-subnet-name" {
   description = "public Subnet name"
 }
+
+variable "routing_mode" {
+  description = "Routing Mode"
+}
+
+variable "auto_create_subnetworks" {
+  type = bool
+}
+
+variable "delete_default_routes" {
+  type = bool
+}
+
+
+variable "route_dest_range" {
+  type        = string
+  description = "destination IP for route"
+}
+
+variable "firewall-allow-name" {
+  type        = string
+  description = "Firewall Name For HTTP request"
+}
+
+variable "firewall-deny-name" {
+  type        = string
+  description = "Firewall Name To Deny SSH"
+}
+
+variable "firewall-allow-protocol" {
+  type        = string
+  description = "protocol for firewall"
+  default     = "tcp"
+}
+
+variable "firewall-allow-port" {
+  type        = list(string)
+  description = "port list"
+  default     = ["80", "8080"]
+
+}
+
+variable "firewall-allow-source" {
+  type    = list(string)
+  default = ["0.0.0.0/0"]
+}
+
+variable "firewall-deny-protocol" {
+  type        = string
+  description = "protocol for firewall"
+  default     = "tcp"
+}
+
+variable "firewall-deny-port" {
+  type        = list(string)
+  description = "port list"
+  default     = ["22"]
+}
+
+variable "firewall-deny-source" {
+  type    = list(string)
+  default = ["0.0.0.0/0"]
+}
+
+variable "target-tag"{
+  type    = list(string)
+}
+ variable "web-route-next-hop-gateway" {
+  type = string
+  default = "default-internet-gateway"
+ }
